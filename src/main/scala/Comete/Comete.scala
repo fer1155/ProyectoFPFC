42cb2d768ce7551ed87ef1f731015d0da93488a3
juan pablo moreno hernandez <juan.moreno.hernandez@correounivalle.edu.co>
2024-11-25 00:18:43 -0500

    correcciones

--- src/main/scala/Comete/Comete.scala
+++ src/main/scala/Comete/Comete.scala
@@ -1,66 +1,82 @@
+import Comete.{DistributionValues, MedidaPol}
+
 import scala.annotation.tailrec
 import scala.math.BigDecimal.double2bigDecimal
 
+// Medida de polarizacion
 package object Comete {
 
   type DistributionValues = Vector[Double]
+  // Representa los valores posibles de opinion
+  // Tipo para los valores reales de una distribucion (Ordenado) [0,0 - 1,0]
 
   type Frequency = Vector[Double]
+  // Representa probabilidades
+  // Pi_k es una frecuencia de longitud k
+  // Si Pi_k.lenght=k, 0 <= Pi_k(i) <= 1, 0 <= i <= k-1
+  // Pi_k.sum == 1
+  // Toman valores reales entre 0 y 1 (No hay orden)
 
   type Distribution = (Frequency, DistributionValues)
+  // (π,y) Probabilidades, valores de distribucion
+  // (Pi,dv) es una distribucion si Pi es una frecuencia y dv son los valores de distribucion
+  // Son de la misma longitud
 
   type MedidaPol = Distribution => Double
+  // Funcion xd
+
+  def min_p(f: Double => Double, min: Double, max: Double, prec: Double): Double =
+    if (max - min < prec) (min + max) / 2
+    else {
+      val step = (max - min) / 10
+      val points = (0 to 10).map(i => min + i * step)
+      val minIndex = points.indices.minBy(i => f(points(i)))
+      val (left, right) = (if (minIndex > 0) points(minIndex - 1) else min,
+        if (minIndex < points.size - 1) points(minIndex + 1) else max)
+
+      if (math.abs((min + max) / 2 - points(minIndex)) < prec) points(minIndex)
+      else min_p(f, left, right, prec)
+    }
 
 
-////  def min_p(f: Double => Double, min: Double, max: Double, prec: Double): Double = {
-////    if (max - min < prec) (min + max) / 2
-////    else {
-////      // Divide el intervalo en 10 partes iguales
-////      val division = (max - min) / 10
-////      val puntosDivision = (0 to 10).map(i => min + i * division).toList
-////
-////      // Encuentra el índice del punto donde f es mínimo
-////      val puntoMinIndex = puntosDivision.indices.minBy(i => f(puntosDivision(i)))
-////
-////      // Identifica el predecesor y el sucesor del punto mínimo
-////      val predecesor = if (puntoMinIndex > 0) puntosDivision(puntoMinIndex - 1) else min
-////      val sucesor = if (puntoMinIndex < puntosDivision.size - 1) puntosDivision(puntoMinIndex + 1) else max
-////
-////      // Si la diferencia entre el mínimo y el punto medio es suficientemente pequeña, retorna el mínimo
-////      if (math.abs((min + max) / 2 - puntosDivision(puntoMinIndex)) < prec) puntosDivision(puntoMinIndex)
-////      else min_p(f, predecesor, sucesor, prec) // Llamada recursiva
-////    }
-////  }
-/////*
-//
-//  //Funcion Auxiliar
-//  def rhoAux(p: Double, freq: Frequency, values: DistributionValues, alpha: Double, beta: Double): Double = {
-//    (freq zip values).map { case (pi, yi) =>
-//      math.pow(pi, alpha) * math.pow(math.abs(yi - p), beta)
-//    }.sum
-//  }
-//  // Función principal para calcular la medida de polarización comete
-//  def rhoCMT_Gen(alpha: Double, beta: Double): MedidaPol = {
-//    (distribution: Distribution) => {
-//      val (freq, values) = distribution
-//      // Función de rhoAux que toma p como variable
-//      val f = (p: Double) => rhoAux(p, freq, values, alpha, beta)
-//      //Intervalo [0, 1] con precisión 0.01
-//      val pmin = min_p(f, 0.0, 1.0, 0.01)
-//      f(pmin)
-//    }
-//  }
-//
-def normalizar(m: MedidaPol): MedidaPol = {
-  // Define el peor caso de polarización (0.5 en los extremos 0 y 1, y 0 en los otros valores)
-  val peorCaso: Frequency = Vector(0.5, 0.0, 0.0, 0.0, 0.5)
-  val valoresDistribucion: DistributionValues = Vector(0.0, 0.25, 0.5, 0.75, 1.0)
-  // Calcula la polarización máxima
-  val maxPolarizacion = m(peorCaso, valoresDistribucion)
-  distribucion => {
-    val resultado = m(distribucion) / maxPolarizacion
-    math.round(resultado * 1000) / 1000.0
+  //Funcion Auxiliar
+  def rhoCMT_Gen(alpha: Double, beta: Double): MedidaPol = {
+    def rhoAux(dist: Distribution, p: Double): Double = {
+      val (frecuencias, valores) = dist
+      frecuencias.zip(valores)
+        .map { case (pi, yi) =>
+          math.pow(pi, alpha) * math.pow(math.abs(yi - p), beta)
+        }.sum
+    }
+
+    def medidaComete(dist: Distribution): Double = {
+      // Función auxiliar que será minimizada
+      val f = (p: Double) => rhoAux(dist, p)
+      // Buscamos el mínimo en el intervalo [0,1] con precisión 0.001
+      val p_min = min_p(f, 0.0, 1.0, 0.001)
+
+      // Devolvemos el valor mínimo encontrado
+      val resultado = rhoAux(dist, p_min)
+      math.round(resultado * 1000) / 1000.0
+    }
+
+    medidaComete
   }
- }
+
+
+  def normalizar(m: MedidaPol): MedidaPol = {
+    // Define el peor caso de polarización (0.5 en los extremos 0 y 1, y 0 en los otros valores)
+    val peorCaso: Frequency = Vector(0.5, 0.0, 0.0, 0.0, 0.5)
+    val valoresDistribucion: DistributionValues = Vector(0.0, 0.25, 0.5, 0.75, 1.0)
+    // Calcula la polarización máxima
+    val maxPolarizacion = m(peorCaso, valoresDistribucion)
+    distribucion => {
+      val resultado = m(distribucion) / maxPolarizacion
+      math.round(resultado * 1000) / 1000.0
+    }
+  }
+
+
+
 }
 //
--- src/main/scala/Opinion/Opinion.scala
+++ src/main/scala/Opinion/Opinion.scala
@@ -35,40 +35,43 @@ package object Opinion {
 
   // Funcion Rho
 
-//  def rho(alpha:Double, beta:Double):AgentsPolMeasure = {
-//    def contarCreenciasEnIntervalos(creencias:SpecificBelief, posiblesValores:DistributionValues):SpecificBelief = {
-//      //Genera los intervalos basados en posiblesValores
-//      val k = posiblesValores.length
-//      val intervalos = (0 until k).map {
-//        case 0 => (0.0, (posiblesValores(1)/2))
-//        case i if i == k - 1 => ((posiblesValores(i - 1) + posiblesValores(i)) / 2, 1.0)
-//        case i => ((posiblesValores(i - 1) + posiblesValores(i)) / 2, (posiblesValores(i) + posiblesValores(i + 1)) / 2)
-//      }.toVector
-//
-//      //Cuenta cuántas creencias caen en cada intervalo
-//      intervalos.zipWithIndex.map { case ((limInf, limSup), indx) =>
-//        if (indx == k - 1)
-//          creencias.count(x => x >= limInf && x <= limSup)  //Último intervalo incluye 1.0
-//        else
-//          creencias.count(x => x >= limInf && x < limSup)
-//      }
-//    }
-//    def construirDistribucion(creencias:SpecificBelief, values:DistributionValues):Frequency = {
-//      //Se cuentan las creencias en intervalos
-//      val creenciasCategorizadas = contarCreenciasEnIntervalos(creencias, values)
-//      //Obtenemos el total de creencias (n agentes)
-//      val totalCreencias = creencias.length.toDouble
-//      //Calculamos la frecuencia dividiendo cada elemento de creenciasCategorizadas entre el total
-//      val frecuencia = creenciasCategorizadas.map(_ / totalCreencias)
-//      frecuencia
-//    }
-//    (creencias:SpecificBelief, values:DistributionValues) => {
-//      val cmt = rhoCMT_Gen(alpha, beta)
-//      val distribucion = construirDistribucion(creencias, values)
-//      val cmt1norm = normalizar(cmt)
-//      cmt1norm(distribucion, values)
-//    }
-//  }
+  def rho(alpha: Double, beta: Double): AgentsPolMeasure = {
+    def contarCreenciasEnIntervalos(creencias: SpecificBelief, posiblesValores: DistributionValues): SpecificBelief = {
+      //Genera los intervalos basados en posiblesValores
+      val k = posiblesValores.length
+      val intervalos = (0 until k).map {
+        case 0 => (0.0, (posiblesValores(1) / 2))
+        case i if i == k - 1 => ((posiblesValores(i - 1) + posiblesValores(i)) / 2, 1.0)
+        case i => ((posiblesValores(i - 1) + posiblesValores(i)) / 2, (posiblesValores(i) + posiblesValores(i + 1)) / 2)
+      }.toVector
+
+      //Cuenta cuántas creencias caen en cada intervalo
+      intervalos.zipWithIndex.map { case ((limInf, limSup), indx) =>
+        if (indx == k - 1)
+          creencias.count(x => x >= limInf && x <= limSup) //Último intervalo incluye 1.0
+        else
+          creencias.count(x => x >= limInf && x < limSup)
+      }
+    }
+
+    def construirDistribucion(creencias: SpecificBelief, values: DistributionValues): Frequency = {
+      //Se cuentan las creencias en intervalos
+      val creenciasCategorizadas = contarCreenciasEnIntervalos(creencias, values)
+      //Obtenemos el total de creencias (n agentes)
+      val totalCreencias = creencias.length.toDouble
+      //Calculamos la frecuencia dividiendo cada elemento de creenciasCategorizadas entre el total
+      val frecuencia = creenciasCategorizadas.map(_ / totalCreencias)
+      frecuencia
+    }
+
+    (creencias: SpecificBelief, values: DistributionValues) => {
+      val cmt = rhoCMT_Gen(alpha, beta)
+      val distribucion = construirDistribucion(creencias, values)
+      val cmt1norm = normalizar(cmt)
+      val resultado = cmt1norm(distribucion, values)
+      math.round(resultado * 1000) / 1000.0
+    }
+  }
 
 
   // Tipos para modelar la evolución de la opinión en una red
@@ -76,7 +79,9 @@ package object Opinion {
   type SpecificWeightedGraph = (WeightedGraph, Int)
   type GenericWeightedGraph = Int => SpecificWeightedGraph
 
+
   type FunctionUpdate = (SpecificBelief, SpecificWeightedGraph) => SpecificBelief
+
 
   def confBiasUpdate(b: SpecificBelief, swg: SpecificWeightedGraph): SpecificBelief = {
     val n = b.length
@@ -98,20 +103,30 @@ package object Opinion {
     }
   }
 
-  def showWeightedGraph(swg: SpecificWeightedGraph): IndexedSeq[IndexedSeq[Double]] = {
-  ...
-  }
+  //  def showWeightedGraph(swg: SpecificWeightedGraph): IndexedSeq[IndexedSeq[Double]] = {
+  //    //Extrae la función de influencia y el número de agentes del SpecificWeightedGraph
+  //    val (graphFunc, nags) = swg
+  //    //Crea una matriz de influencias usando dos bucles for, donde:
+  //    // - El bucle externo itera sobre el índice i, que representa el agente de origen
+  //    // - El bucle interno itera sobre el índice j, que representa el agente de destino
+  //    // Para cada par (i, j), calcula la influencia usando graphFunc(i, j) y almacena el resultado en la matriz.
+  //    for (i <- 0 until nags) yield {
+  //      for (j <- 0 until nags) yield graphFunc(i, j)
+  //    }
+  //  }
+  //
 
-  def simulate(fu: FunctionUpdate, swg: SpecificWeightedGraph, b0: SpecificBelief, t: Int): IndexedSeq[SpecificBelief] = {
-  ...
-  }
-
-  // Versiones paralelas
-  def rhoPar(alpha: Double, beta: Double): AgentsPolMeasure = {
-    // rho es la medida de polarización de agentes basada en comete
-  ...
-  }
-
+  //
+  //  def simulate(fu: FunctionUpdate, swg: SpecificWeightedGraph, b0: SpecificBelief, t: Int): IndexedSeq[SpecificBelief] = {
+  //  ...
+  //  }
+  //
+  //  // Versiones paralelas
+  //  def rhoPar(alpha: Double, beta: Double): AgentsPolMeasure = {
+  //    // rho es la medida de polarización de agentes basada en comete
+  //  ...
+  //  }
+  //
   def confBiasUpdatePar(n: Int, b: Vector[Double], swg: (Int, Int) => Double): Vector[Double] = {
     // Función para calcular la nueva creencia de un agente
     def actualizarCreenciaParaAgente(i: Int): Double = {
